13b5372afb710f06843e0cb2a483f31af70b9c05
Grant <grant.schoffelen@gmail.com>
2014-10-26 19:46:44 -0400

    Added new javascript file with full specs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+//global variables- working 
+    var mystery_num = Math.floor((Math.random() * 100) + 1)
+	var guess = $("#guessbox").val();
+	var guessed = [];  
+    var counter = 0; 
+
 
 //new game function starts new game -working 
 $('#newgame').on('click', function(){
@@ -13,80 +19,95 @@ $('#newgame').on('click', function(){
     $('body').addClass('playing')
 	})
 
-
-//global variables- working 
-    var mystery_num = Math.floor((Math.random() * 100) + 1)
-	var guess = $("#guessbox").val();
-	var guessed = [];  
-    var counter = 0; 
-
 $(document).ready(function(){
 
 //Checks if users_ipunt is a valid guess
 	$("#guess_submit").on("click", function valid_guess(){
 		guess = $("#guessbox").val();
+		$("#guessbox").val(""); 
 		var proxy =Math.abs(mystery_num - guess); 
-
-		for(var i=0; i<guessed.length; i++){
-			if (guess=== guessed[i]){
-				alert('You already gussed that number')
-			}
-		}
-
-
-
-		if(isNaN(guess)){
-		alert("Please enter a Number")
-		}
-		else if (guess>100 || guess <1 || guess ===0){
-		alert("please enter a number between 1 and 100")
-		}
-		else if (guess > mystery_num){
-		$("#game_text").text("Guess Lower")
-		guessed.push(guess); 
-		counter ++; 
 		
 
-		}
 
-		else if (guess < mystery_num){
-		$("#game_text").text("Guess Higher")
-		guessed.push(guess); 
-		counter ++;
-		}
+		if( counter < 5){
+			for(var i=0; i<guessed.length; i++){
+				if (guess=== guessed[i]){
+					alert('You already gussed that number')
+					counter= counter -1; 
+				}
+			}
 
-		else if (guess == mystery_num){
-		$("#game_text").text("Your on the Money!")
-		$('body').removeClass('playing'); 
-		$('body').addClass('winner')
-		}
 
-	
-	
+			if(isNaN(guess)){
+			alert("Please enter a Number")
+			}
+			else if (guess>100 || guess <1 || guess ===0){
+			alert("please enter a number between 1 and 100")
+			}
+			else if (guess > mystery_num){
+			$("#game_text").text("Guess Lower")
+			guessed.push(guess); 
+			counter ++; 
+			}
 
-		if (proxy>30 && proxy<100){
-			$('#proxy').text('You are Ice cold')
-		}
-		else if (proxy>=15 && proxy<30){
-			$('#proxy').text('You are Cold')
-		}
-		else if (proxy>=7 && proxy<15){
-			$('#proxy').text('You are Warm')
-		}
-		else if (proxy>3 && proxy<7){
-			$('#proxy').text('You Are Hot')
-		}
-		else if (proxy>0 && proxy<=3){
-			$('#proxy').text('You Are Boiling Hot')
-		}
-		else if (proxy===0){
-			$('#proxy').text('Congratulations')
-		}
+			else if (guess < mystery_num){
+			$("#game_text").text("Guess Higher")
+			guessed.push(guess); 
+			counter ++;
+			}
+
+			else if (guess == mystery_num){
+			$("#game_text").text("Your on the Money!")
+			$('body').removeClass('playing'); 
+			$('body').addClass('winner')
+			$('#count').text('You Won!')
+			}
+
+		
+		
+
+			if (proxy>30 && proxy<100){
+				$('#proxy').text('You are Ice cold')
+			}
+			else if (proxy>=15 && proxy<30){
+				$('#proxy').text('You are Cold')
+			}
+			else if (proxy>=7 && proxy<15){
+				$('#proxy').text('You are Warm')
+			}
+			else if (proxy>3 && proxy<7){
+				$('#proxy').text('You Are Hot')
+			}
+			else if (proxy>0 && proxy<=3){
+				$('#proxy').text('You Are Boiling Hot')
+			}
+			else if (proxy===0){
+				$('#proxy').text('Congratulations')
+			}
+             var proxy1 = Math.abs(mystery_num - guessed[i-1]);
+             if(proxy1<proxy){
+             	$('#last_guess').text("Your guess " + guess + " is further than your guess " + guessed[i-1])
+             }
+             else if (proxy1>proxy){
+             	$('#last_guess').text("You're guess " + guess + " is closer than your guess " + guessed[i-1])
+
+             }
+			$('#count').text(5-counter + " Guesses Remaining")
+			
+			$('#guess_list').text("Previous Guesses " + guessed )
+			
+
+
 
 		//alert(guessed); 
         //alert(counter); 
 
-         
+     }    
+
+     else (
+     	alert('Your done Press Start new game to start over'))
+
+    
 
  
 	}); 
